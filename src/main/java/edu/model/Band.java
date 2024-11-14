b2ad8d048beadd195b99276e4a1a9928ba8f12e2
LeshanieM <Leshaniebiz@gmail.com>
2024-11-14 20:25:09 +0530

    modal created

--- src/main/java/edu/model/Band.java
+++ src/main/java/edu/model/Band.java
@@ -1,4 +1,16 @@
 package edu.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@ToString
+@NoArgsConstructor
 public class Band {
+    private Integer id;
+    private String bandName;
+    private Double price;
 }
