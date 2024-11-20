c97bf6d267be8264955f71b66cc36911bef96240
LeshanieM <Leshaniebiz@gmail.com>
2024-11-20 21:11:57 +0530

    catering created

---
+++ src/main/java/edu/model/Catering.java
@@ -0,0 +1,18 @@
+package edu.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@ToString
+@NoArgsConstructor
+public class Catering {
+    private Integer id;
+    private String cateringType;
+    private Integer price;
+    private String date;
+
+}
